1f4638c3b33eabd511bcd365d9c00b72560e2b1c
Robin Ricard <ricard.robin@gmail.com>
2014-07-07 15:52:00 +0200

    Lib funcs

---
+++ index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var mongoose = require('mongoose');
+var async = require('async');
+var ObjectId = mongoose.Types.ObjectId;
+
+function capitalize(string)
+{
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+module.exports = function(seedObject, cb) {
+  async.each(Object.keys(seedObject), function(mongoModel, cb) {
+    var documents = seedObject[mongoModel];
+    mongoModel = capitalize(mongoModel);
+    var Model = mongoose.model(mongoModel);
+    async.each(Object.keys(documents), function(documentId, cb) {
+      var document = documents[documentId];
+      Model.update(
+        { _id: new ObjectId(documentId) },
+        document,
+        { upsert: true },
+        cb
+      );
+    }, cb);
+  }, cb);
+};
